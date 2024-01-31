f6fcfc1fc156cc6827b0083e0f5a725f54441b15
Kirill Soldatov <k.igorevitch88@yandex.ru>
2024-01-31 14:59:33 +0300

    Added basic components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
+import {Technologies} from "./components/Technologies";
+import {Header} from "./components/Header";
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!!
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="App">
+            <Header />
+            <Technologies />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <div>
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">News Feed</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+            </ul>
+        </div>
+    );
+};
