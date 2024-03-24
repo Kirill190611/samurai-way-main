a05ea96f6932f904a637f059e7711aa37a10c5e7
Kirill Soldatov <k.igorevitch88@yandex.ru>
2024-03-24 09:39:35 +0300

    Code refactoring for dialogs component

--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -1,38 +1,57 @@
 import React from 'react';
 import classes from './Dialogs.module.css';
 
-type DialogsProps = {
-
-}
+type DialogsProps = {}
 
 export const Dialogs = (props: DialogsProps) => {
     return (
         <div className={classes.dialogs}>
-            <ul className={classes.dialogs_friends_container}>
-                <li>Kirill</li>
-                <li>Kirill</li>
-                <li>Kirill</li>
-                <li>Kirill</li>
-                <li>Kirill</li>
-                <li>Kirill</li>
-            </ul>
+            <h3>My dialogs</h3>
             <div className={classes.dialogs_container}>
-                <h3>My dialogs</h3>
-                <ul>
-                    <li>
+                <ul className={classes.dialogs_list}>
+                    <li className={classes.dialogs_user}>
                         <div>Avatar</div>
                         <div>Name</div>
-                        <div>Message</div>
                     </li>
-                    <li>
+                    <li className={classes.dialogs_user}>
                         <div>Avatar</div>
                         <div>Name</div>
-                        <div>Message</div>
                     </li>
-                    <li>
+                    <li className={classes.dialogs_user}>
                         <div>Avatar</div>
                         <div>Name</div>
-                        <div>Message</div>
+                    </li>
+                    <li className={classes.dialogs_user}>
+                        <div>Avatar</div>
+                        <div>Name</div>
+                    </li>
+                    <li className={classes.dialogs_user}>
+                        <div>Avatar</div>
+                        <div>Name</div>
+                    </li>
+                    <li className={classes.dialogs_user}>
+                        <div>Avatar</div>
+                        <div>Name</div>
+                    </li>
+                </ul>
+                <ul className={classes.dialogs_messages}>
+                    <li className={classes.dialogs_message}>
+                        Message
+                    </li>
+                    <li className={classes.dialogs_message}>
+                        Message
+                    </li>
+                    <li className={classes.dialogs_message}>
+                        Message
+                    </li>
+                    <li className={classes.dialogs_message}>
+                        Message
+                    </li>
+                    <li className={classes.dialogs_message}>
+                        Message
+                    </li>
+                    <li className={classes.dialogs_message}>
+                        Message
                     </li>
                 </ul>
             </div>
