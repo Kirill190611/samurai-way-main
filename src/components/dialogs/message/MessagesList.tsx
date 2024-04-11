9b79f67090451715c3ed6d8020b441fb2fb171f3
Kirill Soldatov <k.igorevitch88@yandex.ru>
2024-04-11 22:17:40 +0300

    Added 1st callback for practice with useRef hook

--- src/components/dialogs/message/MessagesList.tsx
+++ src/components/dialogs/message/MessagesList.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import classes from "../Dialogs.module.css";
 import {MessageItem} from "./MessageItem";
 import {MessageProps} from "../../../redux/State";
+import {useRef} from "react";
 
 type MessagesListProps = {
     messages: Array<MessageProps>
@@ -9,13 +10,27 @@ type MessagesListProps = {
 export const MessagesList = ({
                                  messages
                              }: MessagesListProps) => {
+    const newMessageElement = useRef<HTMLTextAreaElement>(null)
+
+    const onAddMessage = () => {
+        if (newMessageElement.current !== null) {
+            alert(newMessageElement.current.value)
+        }
+    }
+
     return (
-        <ul className={classes.dialogs_messages}>
+        <div className={classes.dialogs_message__container}>
+            <ul className={classes.dialogs_messages}>
 
-            {messages.map(message => <MessageItem id={message.id}
-                                                  message={message.message}
-                                                  isFriendMessage={message.isFriendMessage}/>)}
+                {messages.map(message => <MessageItem id={message.id}
+                                                      message={message.message}
+                                                      isFriendMessage={message.isFriendMessage}/>)}
 
-        </ul>
+            </ul>
+            <form className={classes.dialogs_addMessage} action="#">
+                <textarea ref={newMessageElement} placeholder="your news"></textarea>
+                <button onClick={onAddMessage}>Add message</button>
+            </form>
+        </div>
     );
 };
--- src/components/profile/myPosts/post/MyPosts.tsx
+++ src/components/profile/myPosts/post/MyPosts.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import classes from './MyPosts.module.css'
 import {PostList} from "./PostList";
 import {PostProps} from "../../../../redux/State";
@@ -6,15 +6,23 @@ import {PostProps} from "../../../../redux/State";
 type MyPostsProps = {
     posts: Array<PostProps>
 }
+
 export const MyPosts = ({posts}: MyPostsProps) => {
+    const newPostElement = useRef<HTMLTextAreaElement>(null)
+    const onAddPost = () => {
+        if (newPostElement.current !== null) {
+            let text = newPostElement.current.value;
+            alert(text);
+        }
+    }
 
     return (
         <div>
             <h3>My posts</h3>
             <form className={classes.form}
                   action="#">
-                <textarea placeholder="your news"></textarea>
-                <button type="submit">Add post</button>
+                <textarea ref={newPostElement} placeholder="your news"></textarea>
+                <button onClick={onAddPost}>Add post</button>
             </form>
             <PostList postsData={posts}/>
         </div>
