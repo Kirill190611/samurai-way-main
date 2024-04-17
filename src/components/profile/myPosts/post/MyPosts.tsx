7e1d07e3c1d26037970b4066bca0ceb24e040703
Kirill Soldatov <k.igorevitch88@yandex.ru>
2024-04-17 21:59:39 +0300

    Added logic for added posts and callback to MyPosts component

--- src/components/profile/myPosts/post/MyPosts.tsx
+++ src/components/profile/myPosts/post/MyPosts.tsx
@@ -1,4 +1,4 @@
-import React, {useRef, useState} from 'react';
+import React, {ChangeEvent, useRef, useState} from 'react';
 import classes from './MyPosts.module.css'
 import {PostList} from "./PostList";
 import {addPost, PostProps, state} from "../../../../redux/State";
@@ -13,20 +13,28 @@ export const MyPosts = ({
                             addPost
                         }: MyPostsProps) => {
     const newPostElement = useRef<HTMLTextAreaElement>(null)
+
+    const [value, setValue] = useState("")
     const onAddPost = () => {
         if (newPostElement.current !== null) {
             addPost(newPostElement.current.value);
         }
     }
 
+    const onChangeHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        setValue(e.currentTarget.value)
+    }
+
     return (
         <div>
             <h3>My posts</h3>
-            <form className={classes.form}
-                  action="#">
-                <textarea ref={newPostElement} placeholder="your news"></textarea>
+            <div className={classes.form}>
+                <textarea value={value}
+                          ref={newPostElement}
+                          onChange={onChangeHandler}
+                          placeholder="your news"></textarea>
                 <button onClick={onAddPost}>Add post</button>
-            </form>
+            </div>
             <PostList postsData={posts}/>
         </div>
     );
