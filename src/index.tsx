b7d8d47dd6a7d778ab7038fb8da29abd39c0ae36
Kirill Soldatov <k.igorevitch88@yandex.ru>
2024-05-13 20:42:46 +0300

    Code refactoring

--- src/index.tsx
+++ src/index.tsx
@@ -6,17 +6,9 @@ import App from './App';
 import {store} from "./redux/redux-store";
 import {Provider} from "react-redux";
 
-let rerenderEntireTree = () => {
-    ReactDOM.render(
-        <Provider store={store}>
-            <App/>
-        </Provider>,
-        document.getElementById('root')
-    );
-}
-
-rerenderEntireTree();
-
-store.subscribe(() => {
-    rerenderEntireTree();
-});
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
+)
