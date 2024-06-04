80967010036e5424086dfecf9a50ae3a8a3d0ae0
Kirill Soldatov <k.igorevitch88@yandex.ru>
2024-06-04 23:40:52 +0300

    Added additional logic

--- src/components/friends/Friends.tsx
+++ src/components/friends/Friends.tsx
@@ -1,8 +1,70 @@
 import React from 'react';
 import classes from './Friends.module.css';
-import {FriendsStateProps} from "../../redux/Store";
+import {FriendsListProps} from "../../redux/Store";
+import {v1} from "uuid";
 
-export const Friends = ({friends}: FriendsStateProps) => {
+type FriendsProps = {
+    friends: FriendsListProps[]
+    follow: (userId: string) => void
+    unfollow: (userId: string) => void
+    setFriends: (friends: FriendsListProps[]) => void
+}
+
+export const Friends = ({
+                            friends,
+                            follow,
+                            unfollow,
+                            setFriends
+                        }: FriendsProps) => {
+    if (friends.length === 0 ) {
+        setFriends([
+        {
+            id: v1(),
+            name: "Dmitriy K.",
+            avatar: "Avatar1",
+            address: {
+                country: "Belarus",
+                city: "Minsk",
+            },
+            status: "I am looking fir a Job right now...",
+            followed: true,
+        },
+        {
+            id: v1(),
+            name: "Svetlana D.",
+            avatar: "Avatar2",
+            address: {
+                country: "Belarus",
+                city: "Minsk",
+            },
+            status: "I am so pretty",
+            followed: true,
+        },
+        {
+            id: v1(),
+            name: "Kirill S.",
+            avatar: "Avatar3",
+            address: {
+                country: "Russia",
+                city: "Obninsk",
+            },
+            status: "I like football!",
+            followed: false,
+        },
+        {
+            id: v1(),
+            name: "Andrew T.",
+            avatar: "Avatar4",
+            address: {
+                country: "USA",
+                city: "LA",
+            },
+            status: "I am free to help you to create good Video Production!",
+            followed: false,
+        },
+    ])
+    }
+
     return (
         <div className={classes.friends}>
             <h3>Friends</h3>
@@ -14,13 +76,23 @@ export const Friends = ({friends}: FriendsStateProps) => {
                                 <li className={classes.friends_item} key={friend.id}>
                                     <div className={classes.friends_imageContainer}>
                                         <div className={classes.friends_avatar}>{friend.avatar}</div>
-                                        <button>{friend.followed ? "Follow" : "Unfollow"}</button>
+                                        {friend.followed
+                                            ? <button onClick={() => {
+                                                unfollow(friend.id)
+                                            }}>Follow</button>
+                                            : <button onClick={() => {
+                                                follow(friend.id)
+                                            }}>Unfollow</button>}
                                     </div>
-                                    <div>
-                                        <div>{friend.name}</div>
-                                        <div>{friend.address.city}</div>
-                                        <div>{friend.address.country}</div>
-                                        <p>{friend.status}</p>
+                                    <div className={classes.friends_infoContainer}>
+                                        <div>
+                                            <div>{friend.name}</div>
+                                            <p>{friend.status}</p>
+                                        </div>
+                                        <div>
+                                            <div>{friend.address.city}</div>
+                                            <div>{friend.address.country}</div>
+                                        </div>
                                     </div>
                                 </li>
                             )
